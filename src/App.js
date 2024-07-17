095cf84faffacbdb7b9fb7f10c3069b555c792e5
Deepanshu0881 <deepanshu.niku@gmail.com>
2024-07-17 23:03:50 +0530

    Home page created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import NavBar from "./components/NavBar,jsx"
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <NavBar/>
+      <Home/>
     </div>
    );
 }
